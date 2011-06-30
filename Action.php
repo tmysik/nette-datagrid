dd4fabf1a47db40e1acf99e96193388607332a5b
MartyIX <vseticka.martin@gmail.com>
2011-06-30 19:34:24 +0800

    cleanup

--- Action.php
+++ Action.php
@@ -34,8 +34,7 @@ class Action extends Nette\ComponentModel\Component implements IAction
 	/** @var Nette\Callback|Closure */
 	public $ifDisableCallback;
 
-        private $keyInLink;
-        private $additionalKeys = array();
+        private $linkArgs = array();
         
 	/**
 	 * Data grid action constructor.
@@ -43,7 +42,7 @@ class Action extends Nette\ComponentModel\Component implements IAction
 	 * @note   presenter or action and must be ended with exclamation mark (!)
 	 *
 	 * @param  string  textual title
-	 * @param  string  textual link destination
+	 * @param  string|array  textual link destination or link 
 	 * @param  Nette\Utils\Html    element which is added to a generated link
 	 * @param  bool    use ajax? (add class self::$ajaxClass into generated link)
 	 * @param  mixed   generate link with argument? (if yes you can specify name of parameter
@@ -53,24 +52,14 @@ class Action extends Nette\ComponentModel\Component implements IAction
 	public function __construct($title, $destination, Nette\Utils\Html $icon = NULL, $useAjax = FALSE, $key = self::WITH_KEY)
 	{
 		parent::__construct();
-		$this->destination = $destination;
 		$this->key = $key;
 
                 if (is_string($destination)) {
                     $this->destination = $destination;
                 } elseif (is_array($destination)) {
-                    $this->destination = $destination[0];
-                    unset($destination[0]);
-                    $this->additionalKeys = $destination;
+                    $this->destination = $destination[0];                    
+                    $this->linkArgs = array_slice($destination, 1);
                 }        
-                
-                if (is_array($key)) {
-                    $this->key = $key[0];
-                    $this->keyInLink = $key[1];
-                } else {
-		    $this->key = $key;
-                    $this->keyInLink = $key;
-                }                
                 
 		$a = Nette\Utils\Html::el('a')->title($title);
 		if ($useAjax) $a->addClass(self::$ajaxClass);
@@ -98,14 +87,9 @@ class Action extends Nette\ComponentModel\Component implements IAction
 			$link = $control->link($this->destination); break;
 		case self::WITH_KEY:
 		default:
-			$key = $this->key == NULL || is_bool($this->key) ? $dataGrid->keyName : $this->key;
-                    
-                        if (count($this->additionalKeys) == 0) {
-                            $link = $control->link($this->destination, array($key => $args[$dataGrid->keyName])); break;
-                        } else {                
-                            $this->additionalKeys[$this->keyInLink] = $args[$dataGrid->keyName];                
-                            $link = $control->link($this->destination, $this->additionalKeys); break;   
-                        }
+			$key = $this->key == NULL || is_bool($this->key) ? $dataGrid->keyName : $this->key;                    
+                        $this->linkArgs[$key] = $args[$dataGrid->keyName];                    
+                        $link = $control->link($this->destination, $this->linkArgs); break;                          
 		}
 
 		$this->html->href($link);
